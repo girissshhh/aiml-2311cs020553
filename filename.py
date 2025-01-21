3e25d5de3096a8c03b75a451367e54a0bac877b8
girissshhh <girissshh22@gmail.com>
2025-01-21 07:09:55 +0000

    arr

---
+++ filename.py
@@ -0,0 +1,11 @@
+import numpy as np
+
+arr = np.array([[1,2,3,4,5], [6,7,8,9,10],[2,12,23,10,89]])
+print(arr)
+print('0th element on 2nd row: ',arr[1,3])
+
+import numpy as np
+
+arr = np.array([[[1, 2, 3], [4, 5, 6]], [[7, 8, 9], [10, 11, 12]]])
+print (arr)
+print (arr[1, 1, 2])
